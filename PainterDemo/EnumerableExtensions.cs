060a0aa0b09983805584f5d7f8ef5f2bc44ea78f
Tom <tom.kelly@twentysixdigital.com>
2018-03-22 17:07:49 +0000

    Modified WithMinimum to only call criterion once

--- PainterDemo/EnumerableExtensions.cs
+++ PainterDemo/EnumerableExtensions.cs
@@ -12,11 +12,11 @@ namespace PainterDemo
   public static class EnumerableExtensions
   {
     public static T WithMinimum<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion)
-      where T : class
-      where TKey : IComparable<TKey> =>
-          sequence.Aggregate((T)null, (best, cur) =>
-          best == null ||
-          criterion(cur).CompareTo(criterion(best)) < 0 ?
-          cur : best);
+      where T : class 
+      where TKey : IComparable<TKey> => 
+        sequence.Select(obj => 
+          Tuple.Create(obj, criterion(obj)))
+            .Aggregate((Tuple<T, TKey>)null, (best, cur) => best == null || cur.Item2.CompareTo(best.Item2) < 0 ? cur : best)
+            .Item1;
   }
 }
