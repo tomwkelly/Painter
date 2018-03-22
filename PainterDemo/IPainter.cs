c664e31d89f4003b1cc70a15850c6101a8ddbb77
Tom <tom.kelly@twentysixdigital.com>
2018-03-22 16:13:39 +0000

    Original bad code

---
+++ PainterDemo/IPainter.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="twentysix" file="IPainter.cs">
+// Copyright (c) twentysix.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace PainterDemo
+{
+  using System;
+
+  public interface IPainter
+  {
+    bool IsAvailable { get; }
+
+    TimeSpan EstimateTimeToPaint(double sqMeters);
+
+    double EstimateCompensation(double sqMeters);
+  }
+}
---
+++ PainterDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PainterDemo
+{
+  class Program
+  {
+
+    private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+    {
+      double bestPrice = 0;
+      IPainter cheapest = null;
+
+      foreach (var painter in painters)
+      {
+        if (painter.IsAvailable)
+        {
+          double price = painter.EstimateCompensation(sqMeters);
+          if (cheapest == null || price < bestPrice)
+          {
+            cheapest = painter;
+          }
+        }
+      }
+
+      return cheapest;
+    }
+
+    static void Main(string[] args)
+    {
+    }
+  }
+}
