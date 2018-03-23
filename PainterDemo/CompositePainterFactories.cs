e032ef7d3dc3a800b1866000fe22fabea87b360c
Tom <tom.kelly@twentysixdigital.com>
2018-03-23 14:04:52 +0000

    BIG ONE --------
    Adding CompositePainter and adding all functionality to create painters with attributes to CompositePainterFactories static class

---
+++ PainterDemo/CompositePainterFactories.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="twentysix" file="CompositePainterFactories.cs">
+// Copyright (c) twentysix.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace PainterDemo
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  static class CompositePainterFactories
+  {
+
+
+    public static IPainter CreateCheapestSelector(IEnumerable<IPainter> painters) =>
+      new CompositePainter<IPainter>(painters,
+        (sqMeters, sequence) => new Painters(sequence).GetAvailable().GetCheapestOne(sqMeters));
+
+
+    public static IPainter CreateFastestSelector(IEnumerable<IPainter> painters) =>
+      new CompositePainter<IPainter>(painters, 
+        (sqMeters, sequence) => new Painters(sequence).GetFastestOne(sqMeters));
+
+    public static IPainter CreateGroup(IEnumerable<ProportionalPainter> painters) => new CompositePainter<ProportionalPainter>(
+      painters,
+      (sqMeters, sequence) =>
+      {
+
+        var time = TimeSpan.FromHours(
+          1 / sequence.Where(painter => painter.IsAvailable)
+            .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours).Sum());
+
+        var cost = sequence.Where(painter => painter.IsAvailable).Select(
+          painter => painter.EstimateCompensation(sqMeters) / painter.EstimateTimeToPaint(sqMeters).TotalHours
+                     * time.TotalHours).Sum();
+
+        return new ProportionalPainter()
+               {
+                 TimePerSqMeter = TimeSpan.FromHours(time.TotalHours / sqMeters),
+                 DollarsPerHour = cost / time.TotalHours
+               };
+      });
+  }
+}
--- PainterDemo/Program.cs
+++ PainterDemo/Program.cs
@@ -9,40 +9,20 @@ namespace PainterDemo
   class Program
   {
 
-    private static IPainter FindCheapestPainter(double sqMeters, Painters painters) => 
-      painters.GetAvailable().GetCheapestOne(sqMeters);
-
-    private static IPainter FindFastestPainter(double sqMeters, Painters painters) => 
-      painters.GetAvailable().GetFastestOne(sqMeters);
-
-    private static IPainter WorkTogether(double sqMeters, IEnumerable<IPainter> painters)
-    {
-      var time = 
-        TimeSpan.FromHours(
-          1/
-      painters
-      .Where(painter => painter.IsAvailable)
-      .Select(painter => 1 / painter.EstimateTimeToPaint(sqMeters).TotalHours)
-      .Sum());
-
-      double cost =
-      painters
-        .Where(painter => painter.IsAvailable)
-        .Select(painter =>
-          painter.EstimateCompensation(sqMeters) /
-          painter.EstimateTimeToPaint(sqMeters).TotalHours *
-          time.TotalHours)
-        .Sum();
-
-      return new ProportionalPainter()
-             {
-               TimePerSqMeter = TimeSpan.FromHours(time.TotalHours/sqMeters),
-               DollarsPerHour = cost/time.TotalHours
-             };
-    }
-
     static void Main(string[] args)
     {
+      var painters = new ProportionalPainter[10];
+
+      IPainter painter;
+
+      // Painters working together
+      painter = CompositePainterFactories.CreateGroup(painters);
+
+      // Cheapest painter
+      painter = CompositePainterFactories.CreateCheapestSelector(painters);
+
+      // Fastest painter
+      painter = CompositePainterFactories.CreateFastestSelector(painters);
     }
   }
 }
